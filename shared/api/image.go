c237ddea76f5bfee3f3e68a285a60881661113bc
--replace-all <saahir.narang@gmail.com>
2025-04-28 03:21:56 +0000

    shared/api: Add field for image type to ImagePost struct
    Signed-off-by: --replace-all <saahir.narang@gmail.com>

--- shared/api/image.go
+++ shared/api/image.go
@@ -57,6 +57,10 @@ type ImagesPost struct {
 	// API extension: image_compression_algorithm
 	CompressionAlgorithm string `json:"compression_algorithm" yaml:"compression_algorithm"`
 
+	// Type of image format
+	// Example: unified, split
+	Type string `json:"type" yaml:"type"`
+
 	// Aliases to add to the image
 	// Example: [{"name": "foo"}, {"name": "bar"}]
 	//
